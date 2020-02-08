8d715f432e361c35761c8de992d44c1f4410030d
pavelgar <pavel.garmuyev@gmail.com>
2020-02-08 12:44:22 +0200

    Update project name

--- src/Program.cs
+++ src/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
+using System.IO;
 
-namespace miniPL_compiler
-{
-  class Program
-  {
-    static void Main(string[] args)
-    {
-      Console.WriteLine("Hello World!");
+namespace miniPL {
+    class Program {
+        static void Main(string[] args) {
+            string[] lines = File.ReadAllLines("./test_program.txt");
+
+            Console.WriteLine("Contents of test_program.txt = ");
+            foreach (string line in lines) {
+                Console.WriteLine("\t" + line);
+            }
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
     }
-  }
 }
